176074cbde522632e480861743161bfa9bd87747
shanayghag <shanayghag200@gmail.com>
2020-08-24 18:21:35 +0530

    Adding albert-base model scripts

---
+++ albert/albert-base/config.py
@@ -0,0 +1,9 @@
+import transformers
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 16
+VALID_BATCH_SIZE = 8
+EPOCHS = 3
+LEARNING_RATE = 3e-05
+TOKENIZER = transformers.AlbertTokenizer.from_pretrained('albert-base-v2')
+MODEL_PATH = "../output/model.bin"
---
+++ albert/albert-base/dataset.py
@@ -0,0 +1,38 @@
+import torch
+
+class CustomDataset:
+
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.abstract = dataframe.ABSTRACT
+        self.targets = self.data.list
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.abstract)
+
+    def __getitem__(self, index):
+        abstract = str(self.abstract[index])
+        abstract = " ".join(abstract.split())
+
+        inputs = self.tokenizer.encode_plus(
+            abstract,
+            None,
+            add_special_tokens = True,
+            max_length = self.max_len,
+            pad_to_max_length = True,
+            return_token_type_ids=True,
+            truncation = True
+        )
+
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs['token_type_ids']
+
+        return{
+                'ids': torch.tensor(ids, dtype=torch.long),
+                'mask': torch.tensor(mask, dtype=torch.long),
+                'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+                'targets': torch.tensor(self.targets[index], dtype=torch.float)
+            }
