b2013542a2e96777e8dfcd7bfafbc1a90102b5b2
Shanay Ghag <shanayghag200@gmail.com>
2021-03-01 21:58:54 +0530

    Model ouput fixed to pooler_output for albert-base

--- albert-base/model.py
+++ albert-base/model.py
@@ -1,15 +1,18 @@
 import torch
 import transformers
 
+
 class AlbertClass(torch.nn.Module):
     def __init__(self):
         super(AlbertClass, self).__init__()
-        self.albert = transformers.AlbertModel.from_pretrained('albert-base-v2')
+        self.albert = transformers.AlbertModel.from_pretrained(
+            'albert-base-v2')
         self.drop = torch.nn.Dropout(0.1)
         self.linear = torch.nn.Linear(768, 6)
-    
+
     def forward(self, ids, mask, token_type_ids):
-        _, output= self.albert(ids, attention_mask = mask)
+        outputs = self.albert(ids, attention_mask=mask)
+        output = outputs.pooler_output
         output = self.drop(output)
         output = self.linear(output)
 
